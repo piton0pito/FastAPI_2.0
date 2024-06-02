6ea2c11f58b0a04ee0d5f7657eaac4598110b73d
nadezhdasilaeva <147171588+nadezhdasilaeva@users.noreply.github.com>
2024-06-02 20:59:32 +0300

    Update login.py

--- app/web/login.py
+++ app/web/login.py
@@ -14,11 +14,6 @@ def login(request: Request):
     return templates.TemplateResponse("login.html", {"request": request})
 
 
-@router.get("/login")
-def login(request: Request):
-    return templates.TemplateResponse("login.html", {"request": request})
-
-
 @router.post("/login")
 async def login(request: Request, db: Session = Depends(get_session)):
     form = await request.form()
@@ -28,16 +23,22 @@ async def login(request: Request, db: Session = Depends(get_session)):
     if len(password) < 6:
         errors.append("Пароль должен состоять от 6 и более символов!")
         return templates.TemplateResponse("login.html", {"request": request, "errors": errors})
-
+    access_token = request.cookies.get("access_token")
+    if access_token:
+        return RedirectResponse(url="/", status_code=302)
     user = db.exec(select(User).where(User.email == email)).first()
     if not user or not user.verify_password(password):
         errors.append("Неверны почта или пароль!")
         return templates.TemplateResponse("login.html", {"request": request, "errors": errors})
     else:
         access_token = create_access_token(data={"sub": user.id})
-        msg = "Вход выполнен!"
-        response = templates.TemplateResponse("login.html", {"request": request, "msg": msg})
+        response = RedirectResponse(url="/", status_code=302)
         response.set_cookie(key="access_token", value=f"Bearer {access_token}", httponly=True)
         return response
 
 
+@router.get("/logout")
+async def logout(request: Request):
+    response = RedirectResponse(url="/", status_code=302)
+    response.delete_cookie(key="access_token")
+    return response
